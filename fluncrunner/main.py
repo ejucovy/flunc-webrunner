ee33138ed73b65fa4c11928ce5e4d62e5238e191
ejucovy <ejucovy@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-12-22 13:55:35 +0000

    refactor to be code-friendly as well as wsgi-friendly
    git-svn-id: https://my-svn.assembla.com/svn/arbez/flunc/webrunner/trunk@1390 e0e46c49-be69-4f5a-ad62-21024a331aea

--- fluncrunner/main.py
+++ fluncrunner/main.py
@@ -3,34 +3,47 @@ from webob.exc import *
 
 import subprocess
 
+
+
 class FluncRunner(object):
+
+    search_path = None
 
     def __init__(self, search_path):
         self.search_path = search_path  # flunc -p option
 
-    def __call__(self, environ, start_response):
-        req = Request(environ)
-
-        suite = req.path_info.strip('/')
+    def execute_tests(self, suite, search_path=None):
         assert ".." not in suite and "/" not in suite
 
+        search_path = search_path or self.search_path
+
         defines = ''
-        for key, val in dict(req.POST).items():
+        for key, val in vars.items():
             if key and val:
                 defines += "%s=%s," % (key, val)
         defines = defines.strip(',')
 
+        # now construct the commandline call to flunc
+        # XXX TODO: flunc should have an API
         args = ['flunc']
-        if self.search_path:
+        if search_path:
             args.extend(['-p', self.search_path])
         if defines:
             args.extend(['-D', defines])
-
         args.append(suite)
 
         print args
 
         ret = subprocess.call(args)
+        return ret
+
+    def __call__(self, environ, start_response):
+        req = Request(environ)
+
+        suite = req.path_info.strip('/')
+        vars = dict(request.POST)
+
+        ret = self.execute_tests(suite, vars)
         
         if ret:
             # TODO: this should be captured, not saved to filesystem
